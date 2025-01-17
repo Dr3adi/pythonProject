e73a8e8d7973453c75829589147d2417ff62105a
astonuser <artem.muxin@gmail.com>
2025-01-17 17:19:04 +0300

    solve task

--- first_step/1.5_input.py
+++ first_step/1.5_input.py
@@ -1,3 +1,20 @@
-a = abs(int(input()))
-b = abs(int(input()))
-print(a + b)
+# a = abs(int(input()))
+# b = abs(int(input()))
+# print(a + b)
+
+
+# способ 1
+# x1 = float(input())
+# x2 = float(input())
+# c = (x2 - x1)
+# print(abs(c))
+
+
+# способ 2
+# x1 = float(input())
+# x2 = float(input())
+# print(max(x1, x2) - min(x1, x2))
+
+
+a = float(input())
+print(round(a, 2), round(a, 3))
